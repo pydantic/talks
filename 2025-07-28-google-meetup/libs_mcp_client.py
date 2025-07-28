969e4e69c1743c81c90d202b1b76bb824c63aa18
Samuel Colvin <s@muelcolvin.com>
2025-07-28 12:24:40 +0100

    remove gh mcp server

--- 2025-07-28-google-meetup/libs_mcp_client.py
+++ 2025-07-28-google-meetup/libs_mcp_client.py
@@ -1,9 +1,8 @@
-import os
 from datetime import date
 
 import logfire
 from pydantic_ai import Agent
-from pydantic_ai.mcp import MCPServerStdio, MCPServerStreamableHTTP
+from pydantic_ai.mcp import MCPServerStdio
 
 logfire.configure(service_name='mcp-client')
 
@@ -12,15 +11,9 @@ logfire.instrument_mcp()
 
 
 pypi_server = MCPServerStdio(command='uv', args=['run', 'pypi_mcp_server.py'])
-github_mcp_pat = os.environ['GITHUB_MCP_PAT']
-github_server = MCPServerStreamableHTTP(
-    url='https://api.githubcopilot.com/mcp/',
-    headers={'authorization': f'Bearer {github_mcp_pat}'},
-    tool_prefix='github',
-)
 libs_agent = Agent(
     'anthropic:claude-sonnet-4-0',
-    toolsets=[github_server, github_server],
+    toolsets=[pypi_server],
     instructions='your job is to help the user research software libraries and packages using the tools provided',
 )
 
@@ -33,8 +26,7 @@ def add_date():
 async def main():
     async with libs_agent:
         libs_agent.set_mcp_sampling_model('anthropic:claude-sonnet-4-0')
-        # result = await libs_agent.run('How many times has pydantic been downloaded this year')
-        result = await libs_agent.run('how many stars does pydantic have?')
+        result = await libs_agent.run('How many times has pydantic been downloaded this year')
     print(result.output)
 
 
