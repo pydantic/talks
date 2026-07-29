60fc7ddcbaf62d7926e78bd0ef86c994d2b5adb5
Samuel Colvin <s@muelcolvin.com>
2026-07-29 13:40:19 +0100

    add websocket codemode hack

--- 2026-07-monty-workshop/world_cup_example/agent.py
+++ 2026-07-monty-workshop/world_cup_example/agent.py
@@ -7,6 +7,8 @@ from pydantic_monty import MountDir
 from .charts import draw_chart
 from .database import Database
 
+# from .hack_websocket_code_mode import WebsocketCodeMode
+
 db = Database()
 
 THIS_DIR = Path(__file__).parent
@@ -33,5 +35,13 @@ agent = Agent(
                 mode='read-write',
             )
         )
+        # WebsocketCodeMode(
+        #     url='ws://localhost:8000',
+        #     mount=MountDir(
+        #         host_path=THIS_DIR / '..' / 'agent_output',
+        #         virtual_path='/output',
+        #         mode='read-write',
+        #     ),
+        # )
     ],
 )
